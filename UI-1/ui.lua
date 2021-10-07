e5a8d26dbcc9008a887ccfd23ac74c587e9e2929
Mapple <84987152+Mapple7777@users.noreply.github.com>
2021-10-07 17:42:32 +0100

    Update ui.lua

--- UI-1/ui.lua
+++ UI-1/ui.lua
@@ -118,46 +118,47 @@ function Library:Create(_HubName,_GameName)
 
     Tabs.Name = "Tabs"
     Tabs.Parent = TabHolder
+
+    local UserInputService = game:GetService("UserInputService")
 	
-local UserInputService = game:GetService("UserInputService")
-    
-    local gui = Main
-    
-    local dragging
-    local dragInput
-    local dragStart
-    local startPos
-    
-    local function update(input)
-    	local delta = input.Position - dragStart
-    	gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-    end
-    
-    gui.InputBegan:Connect(function(input)
-    	if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-    		dragging = true
-    		dragStart = input.Position
-    		startPos = gui.Position
-    
-    		input.Changed:Connect(function()
-    			if input.UserInputState == Enum.UserInputState.End then
-    				dragging = false
-    			end
-    		end)
-    	end
+	local gui = Main
+	
+	local dragging
+	local dragInput
+	local dragStart
+	local startPos
+	
+	local function update(input)
+		local delta = input.Position - dragStart
+		gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+	end
+	
+	gui.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragging = true
+			dragStart = input.Position
+			startPos = gui.Position
+	
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+	end)
+	
+	gui.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			dragInput = input
+		end
+	end)
+	
+	UserInputService.InputChanged:Connect(function(input)
+		if input == dragInput and dragging then
+			update(input)
+		end
     end)
-    
-    gui.InputChanged:Connect(function(input)
-    	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-    		dragInput = input
-    	end
-    end)
-    
-    UserInputService.InputChanged:Connect(function(input)
-    	if input == dragInput and dragging then
-    		update(input)
-    	end
-    end)
+
 
     local _Tabs = {}
     
